fixed energy calculation mistakes

Conditions.py
@@ -2,10 +2,11 @@ import numpy as np
 import Functions
 
 # parameters
-row_num = 2
+row_num = 4
 array_size = row_num * row_num
 islands = list(range(array_size))
-distribute_R = True
+distribute_R = False
+distribute_C = False
 
 # define tunneling parameters
 e = Functions.e
@@ -30,8 +31,12 @@ else:
     R_t_i = [val if idx in set(near_left + near_right) else 0 for idx, val in enumerate(R_i)]
 
 # Capacitance Cond
-Ch = np.random.normal(C, 0, size=(row_num, row_num + 1))
-Cv = np.random.normal(C, 0, size=(row_num + 1, row_num))
+if distribute_C:
+    Ch = np.random.normal(C, C/10, size=(row_num, row_num + 1))
+    Cv = np.random.normal(C, C/10, size=(row_num + 1, row_num))
+else:
+    Ch = np.random.normal(C, 0, size=(row_num, row_num + 1))
+    Cv = np.random.normal(C, 0, size=(row_num + 1, row_num))
 
 diagonal = Ch[:, :-1] + Ch[:, 1:] + Cv[:-1, :] + Cv[1:, :]
 second_diagonal = np.copy(Ch[:, 1:])

FunctionalTest.py
@@ -33,7 +33,6 @@ Vright = 0
 T = 0.001 * e * e / (Cond.C * kB)
 
 # Gillespie parameter, KS statistic value for significance
-KS_boundary = e * 1e-2
 Steady_state_rep = 100
 
 
@@ -207,9 +206,10 @@ def Get_Steady_State():
             I_right, I_down = F.Get_current_from_gamma(Gamma, reaction_index, near_right, near_left)
 
             # solve ODE to update Qg, dQg/dt = (T^-1)(Qg-Qn)
-            Qg = F.Paper_developQ(Qg, dt, Cond.InvTauEigenVectors, Cond.InvTauEigenValues,
-                                  n, Cond.InvTauEigenVectorsInv, Cond.Tau_inv, C_inv, VxCix,
-                                  Rg, Tau, Cg)
+            Qg = F.developQ(Qg, dt, Cond.InvTauEigenVectors, Cond.InvTauEigenValues,
+                            n, Cond.InvTauEigenVectorsInv,
+                            Cond.Tau_inv, C_inv, VxCix,
+                            Rg, Tau, Cg, Cond.matrixQnPart)
 
             # update statistics
             I_avg, I_var = F.update_statistics(I_right, I_avg, I_var, t, dt)
@@ -235,8 +235,9 @@ def Get_Steady_State():
                 # convergence
                 if dist_new - dist > 0:
                     not_decreasing += 1
-                    if not_decreasing > 100000:
-                        print(k, not_decreasing)
+                    #print(dist_new)
+                    if not_decreasing > 10000:
+                        print(k, not_decreasing, std)
                         print(Qg)
                         print(V)
                         raise NameError

Functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import scipy
 import Functions
 
 # parameters
@@ -58,7 +58,7 @@ def return_neighbours(n, I, J):
 
 
 def getVoltage(n, Qg, C_inverse, VxCix):
-    return np.dot(C_inverse, e * n + e * VxCix + Qg)
+    return np.dot(C_inverse, e * n + e * VxCix - Qg)
 
 
 def isNonNegative(x):
@@ -104,6 +104,8 @@ def gamma(dE, Temp, Rt):
 
 def update_statistics(value, avg, n_var, total_time, time_step):
     # from https://github.com/kasirershaharbgu/random_2D_tunneling_arrays/blob/main/random_2d_array_simulation.py#L1957
+    # "Updating the statistics of a measured value according to West's
+    #         algorithm (as described in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2973983/)"
     new_time = total_time + time_step
     dist_from_avg = value - avg
     local_std = dist_from_avg * time_step / new_time