After reroll: -Put integrand calc in Functions -introduced new complete calculation of integrand in F.integrand fixing the E=0 limit, not yet tested -approximate_gamma_integral is now direct gaussian mean -Workss!!!!!!

Conditions.py
@@ -1,5 +1,4 @@
 import numpy as np
-import Functions
 import datetime
 
 # parameters
@@ -7,25 +6,31 @@ row_num = 10
 array_size = row_num * row_num
 islands = list(range(array_size))
 distribute_R = True
-distribute_C = False
+distribute_C = True
 
 # define tunneling parameters
-e = Functions.e
-kB = Functions.kB
-R = 10
+e = 1
+kB = 1
+R = 1
 C = 1
+T0 = 0.001 * e * e / (C * kB)
 
 # define relaxation time parameters
 Cg = 10 * C
 Rg = 100 * R
+Cg_list = [Cg] * array_size
+Rg_list = [Rg] * array_size
+tg = Cg * Rg
 
 near_right = islands[(row_num - 1)::row_num]
 near_left = islands[0::row_num]
 
 if distribute_R:
-    lambd = 2
-    R_t_ij = np.random.exponential(lambd, (array_size, array_size)) * R
-    R_i = np.random.exponential(lambd, array_size) * R
+    stdR = 4.8 * R
+    R_t_ij = 2 ** np.random.uniform(low=np.log2(max(R - stdR, 0.01)),
+                                    high=np.log2(R + stdR), size=(array_size, array_size))
+    R_i = 2 ** np.random.uniform(low=np.log2(max(R - stdR, 0.01)),
+                                 high=np.log2(R + stdR), size=array_size)
     R_t_i = [val if idx in set(near_left + near_right) else 0 for idx, val in enumerate(R_i)]
 else:
     R_t_ij = np.full((array_size, array_size), R)
@@ -33,13 +38,44 @@ else:
     R_t_i = [val if idx in set(near_left + near_right) else 0 for idx, val in enumerate(R_i)]
 
 # Capacitance Cond
+Cix = np.zeros(array_size)
 if distribute_C:
-    sig = 1 / 10
-    Ch = np.random.normal(C, C * sig, size=(row_num, row_num + 1))
-    Cv = np.random.normal(C, C * sig, size=(row_num + 1, row_num))
+    sig = 0.7
+    Ch = np.random.normal(0, C * sig, size=(row_num, row_num + 1))
+    Cv = np.random.normal(0, C * sig, size=(row_num + 1, row_num))
+
+    all_Cs = np.concatenate([Ch.ravel(), Cv.ravel()])
+    if np.all(all_Cs >= 0):
+        pass
+    else:
+        min_val = -np.min(all_Cs) + 0.1
+    Ch, Cv = Ch + min_val + C, Cv + min_val + C
+
+    Cl = np.random.normal(0, C * sig/3, size=(1, array_size))
+    Cr = np.random.normal(0, C * sig/3, size=(1, array_size))
+
+    side_Cs = np.concatenate([Cl.ravel(), Cr.ravel()])
+    if np.all(side_Cs >= 0):
+        pass
+    else:
+        min_val = -np.min(side_Cs) + 0.1
+    Cl, Cr = Cl + min_val + C, Cr + min_val + C/2
+
+    Cix = np.zeros(array_size)
+    for i in near_left:
+        Cix[i] = Cl[0][i]
+    for i in near_right:
+        Cix[i] = Cr[0][i]
+
 else:
     Ch = np.random.normal(C, 0, size=(row_num, row_num + 1))
     Cv = np.random.normal(C, 0, size=(row_num + 1, row_num))
+
+    Cix = np.zeros(array_size)
+    for i in near_left:
+        Cix[i] = np.random.normal(C, 0)
+    for i in near_right:
+        Cix[i] = np.random.normal(C/2, 0)
 
 diagonal = Ch[:, :-1] + Ch[:, 1:] + Cv[:-1, :] + Cv[1:, :]
 second_diagonal = np.copy(Ch[:, 1:])
@@ -49,14 +85,7 @@ second_diagonal = second_diagonal[:-1]
 n_diagonal = np.copy(Cv[1:-1, :])
 C_mat = np.diagflat(diagonal) - np.diagflat(second_diagonal, k=1) - np.diagflat(second_diagonal, k=-1) - \
         np.diagflat(n_diagonal, k=row_num) - np.diagflat(n_diagonal, k=-row_num)
-invC = np.linalg.inv(C_mat)
-C_inverse = np.linalg.inv(C_mat + np.diagflat([Cg] * array_size))  # define inverse
-
-Cix = np.zeros(array_size)
-for i in near_left:
-    Cix[i] = abs(np.random.normal(C, 0))
-for i in near_right:
-    Cix[i] = abs(np.random.normal(C / 2, 0))
+C_inverse = np.linalg.inv(C_mat)  # define inverse
 
 
 def VxCix(Vl, Vr):
@@ -70,19 +99,22 @@ def VxCix(Vl, Vr):
 
 # define tau matrix
 res = C_inverse + np.diagflat([1 / Cg] * array_size)
-Tau_inv = -res / np.repeat(Functions.flattenToColumn(np.array([Rg] * array_size)), res.shape[1], axis=1)
+a = np.array([Rg] * array_size)  # flattening to coloumn
+reshaped = a.reshape((a.size, 1))
+Tau_inv = -res / np.repeat(reshaped, res.shape[1], axis=1)
 
 # tau matrix properties, eigenvalues and default time step
 InvTauEigenValues, InvTauEigenVectors = np.linalg.eig(Tau_inv)
 InvTauEigenVectorsInv = np.linalg.inv(InvTauEigenVectors)
-default_dt = -0.1 / np.min(InvTauEigenValues)
+default_dt = -0.1 / np.min(InvTauEigenValues)  # time in which Qg don't change much
+timeStep = -2 / np.max(InvTauEigenValues)  # time for steady state
 
 # Cg matrix, and Qn calculation
 Tau = np.linalg.inv(Tau_inv)
 matrixQnPart = Tau / (Cg * Rg) - np.eye(Tau.shape[0])
 
 date_ = datetime.datetime.now()
-strin = "parameters_" + date_.strftime("%Y%m%d") + ".txt"
+strin = "parameters_" + date_.strftime("%Y%m%d, %Hh%Mm%Ss") + ".txt"  #
 
 with open(strin, "w") as f:
     f.write("loop parameters" + "\n")
@@ -90,7 +122,7 @@ with open(strin, "w") as f:
     f.write("row_num : " + str(row_num) + "\n")
     f.write("distribute_R : " + str(distribute_R) + "\n")
     if distribute_R:
-        f.write("lambda (exponent) : " + str(lambd) + "\n")
+        f.write("stdR (exponent) : " + str(stdR) + "\n")
     f.write("distribute_C : " + str(distribute_C) + "\n")
     if distribute_C:
         f.write("sig (normal) : " + str(sig) + "\n")
@@ -99,9 +131,12 @@ with open(strin, "w") as f:
     f.write("Cg : " + str(Cg) + "\n")
     f.write("R : " + str(R) + "\n")
     f.write("Rg : " + str(Rg) + "\n")
+    f.write("default_dt : " + str(default_dt) + "\n")
+    f.write("time step : " + str(timeStep) + "\n")
     f.write("---------------------------------------------" + "\n")
     f.write("\n")
     f.write("\n")
 
 print("done")
 print(default_dt)
+print(timeStep)

Functions.py
@@ -1,9 +1,11 @@
 import numpy as np
 from mpmath import quad, ninf, inf, mp, exp, sqrt
 
+import Conditions
+
 # parameters
-kB = 1
-e = 1
+kB = Conditions.kB
+e = Conditions.e
 
 
 # for debugging Warnings may set np.seterr(all='raise')
@@ -69,16 +71,6 @@ def isNonNegative(x):
         return x
 
 
-def taylor(x, T):
-    # 4th degree expansion of x/(1-e^(x*T))
-    return float(
-        T -
-        x / 2 +
-        T * np.power(x, 2) / 12 -
-        np.power(T, 3) * np.power(x, 4) / 720 +
-        np.power(T, 5) * np.power(x, 6) / 30240)
-
-
 def update_statistics(value, avg, n_var, total_time, time_step):
     # from https://github.com/kasirershaharbgu/random_2D_tunneling_arrays/blob/main/random_2d_array_simulation.py#L1957
     # "Updating the statistics of a measured value according to West's
@@ -94,6 +86,7 @@ def update_statistics(value, avg, n_var, total_time, time_step):
 
 
 def Get_current_from_gamma(gamma_list, reaction_index, near_right, near_left):
+    # e == 1
     I_right = 0
     I_down = 0
     for i in range(len(gamma_list)):
@@ -126,90 +119,40 @@ def Get_current_from_gamma(gamma_list, reaction_index, near_right, near_left):
     return I_right, I_down
 
 
-def Paper_developQ(Q, dt, InvTauEigenVec, InvTauEigenVal, n,
-                   InvTauEigenVecInv, InvTau,
-                   C_inverse, VxCix, Rg, Tau, Cg):
-    # Legacy version
+def developQ(Q, dt, n,VxCix):
     # gate charge relaxation, for dQ/dt=inv_tau*Q + b
-    b = -C_inverse.dot(e * n + e * VxCix) / Rg
+    b = -Conditions.Tau_inv.dot(return_Qn_for_n(n, VxCix))
 
     # exponent for time step
-    exponent = np.exp(InvTauEigenVal * dt)
+    exponent = np.exp(Conditions.InvTauEigenValues * dt)
 
     # basis change
-    Q_in_eigenbasis, b = InvTauEigenVecInv.dot(Q), InvTauEigenVecInv.dot(b)
+    Q_in_eigenbasis, b = Conditions.InvTauEigenVectorsInv.dot(Q), Conditions.InvTauEigenVectorsInv.dot(b)
 
     # solution in time
-    Q_new_in_eigenbasis = (exponent * Q_in_eigenbasis) + (b / InvTauEigenVal) * (exponent - 1)
+    Q_new_in_eigenbasis = (exponent * Q_in_eigenbasis) + (b / Conditions.InvTauEigenValues) * (exponent - 1)
 
     # revert to old basis
-    return InvTauEigenVec.dot(Q_new_in_eigenbasis)
+    return Conditions.InvTauEigenVectors.dot(Q_new_in_eigenbasis)
 
 
-def return_Qn_for_n(n, VxCix, Cg, Rg, Tau):
+def return_Qn_for_n(n, VxCix):
     """
     returns Qn for given n vector of array (NxN)
     :param n: (1,N) numpy array
     :param VxCix: (1,N) numpy array
-    :param Cg: (1,N) numpy array
-    :param Rg: (1,N) numpy array
-    :param Tau: (N,N) numpy array
     :return:
     """
-    return Tau.dot((e * n + e * VxCix) / Cg) / Rg - e * n - e * VxCix
-    # return matrixQn.dot((e * n + e * VxCix))
-
-
-def developQ(Q, dt, InvTauEigenVec, InvTauEigenVal, n,
-             InvTauEigenVecInv, InvTau,
-             C_inverse, VxCix, Rg, Tau, Cg, matrixQn):
-    # gate charge relaxation, for dQ/dt=inv_tau*(Q + b), b = -Qn
-    b = -return_Qn_for_n(n, VxCix, Cg, Rg, Tau)
-
-    # exponent for time step
-    exponent = np.exp(InvTauEigenVal * dt)
-
-    # basis change
-    Q_in_eigenbasis, b_in_eigenbasis = InvTauEigenVecInv.dot(Q), InvTauEigenVecInv.dot(b)
-
-    # solution in time
-    Q_new_in_eigenbasis = exponent * (Q_in_eigenbasis + b_in_eigenbasis)
-
-    # revert to old basis
-    return InvTauEigenVec.dot(Q_new_in_eigenbasis) - b
+    # sum = Tau.dot(n_prime / Cg) / Rg
+    # #return sum - n_prime
+    n_prime = e * n + e * VxCix
+    return Conditions.matrixQnPart.dot(n_prime)
 
 
 def getWork(i, j, C_inv, curr_V):
     Work = e * (2 * curr_V[j] + e * C_inv[j][i] - e * C_inv[j][j] -
                 (2 * curr_V[i] + e * C_inv[i][i] - e * C_inv[i][j])) / 2
     return Work
-
-
-def high_impedance_p(x, mu, Temp):
-    """
-    P- function for high impedance.
-    :param x: function input (energy) == E+dE.
-    :param mu: Electrostatic energy of environment == Ec.
-    :param Temp: Temperature.
-    :return: P(x)
-    """
-    sigma_squared = 2 * mu * Temp
-    mu = -mu
-    return exp(-(x - mu) ** 2 / (2 * sigma_squared)) / sqrt(2 * np.pi * sigma_squared)
-
-
-def integrand_gauss(x, temperature, value, Ec):
-    if x == 0:
-        return temperature
-    result = high_impedance_p(x + value, Ec, temperature) * x / (1 - exp(-x / temperature))
-    return result
-
-
-def make_integrand(temp1, val1, Ec1):
-    def f1(x):
-        return integrand_gauss(x, temp1, val1, Ec=Ec1)
-
-    return f1
 
 
 def integrand(T, dE, Ec):
@@ -220,16 +163,17 @@ def integrand(T, dE, Ec):
         :param T: Temperature.
         :return: P(E)*E*f_BE(-E)
         """
+
     def conv(E):
         if np.abs(E) < 1e-8:
-            zero_limit_gauss = exp(-(dE + Ec) ** 2 / (4*Ec*T))
-            return zero_limit_gauss * sqrt(T/(4*np.pi*Ec))
+            zero_limit_gauss = exp(-(dE + Ec) ** 2 / (4 * Ec * T))
+            return zero_limit_gauss * sqrt(T / (4 * np.pi * Ec))
 
-        gauss = exp(-(E + dE + Ec) ** 2 / (4*Ec*T))
-        gauss /= sqrt(np.pi * 4*Ec*T)
+        gauss = exp(-(E + dE + Ec) ** 2 / (4 * Ec * T))
+        gauss = gauss / sqrt(np.pi * 4 * Ec * T)
 
         bose_mean = E / (1 - exp(-E / T))
 
-        return bose_mean*gauss
+        return bose_mean * gauss
 
     return conv