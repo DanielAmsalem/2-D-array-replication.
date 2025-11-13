added full expected list construction: - need to fix multiproccesing on cluster - need to build table multiprocessing computation

mp_compute/main_TPmeas.py
@@ -9,7 +9,6 @@ matplotlib.use("TkAgg")
 
 import matplotlib.pyplot as plt
 import numpy as np
-import bisect
 
 from models import Export, SteadyStateResult
 from gamma_functions import Get_Steady_State
@@ -19,23 +18,42 @@ from preparation import (
     prepare_table_triplets,
     output_table_triplets,
 )
+import Functions as F
 import curve_plotter
+
+from dataclasses import asdict
+import orjson
+import csv
 
 EXPORT_PATH = Path(__file__).parent.parent / "export"
 
 
 def main(export: Export) -> None:
-    loop_count = 3
+    loop_count = 5
     T0_unitless = 0.001
-    T0_std = T0_unitless / 20
+    T_std = 1/20
     init = prepare_initial_state(loop_count=loop_count, unitless_T0=T0_unitless)
 
     date_ = datetime.datetime.now()
-    run_name = date_.strftime("%Y_%m_%d_%H_%M_%S")
+    run_name = date_.strftime("%Y%m%d, %Hh%Mm%Ss")
 
-    if not validate_table_triplets_file(export.prepare_table_triplets_file, init, np.array([init.T0])):
-        expected_list = [T0_unitless + i * T0_std for i in range(init.row_num)]
-        table_triplets = prepare_table_triplets(init, expected_list)
+    ### report parameters of run to report file
+    outfile = Path(EXPORT_PATH / f"{run_name}.json")
+    raw_fields = asdict(init)
+    serialized_init_data = orjson.dumps(raw_fields, option=orjson.OPT_SERIALIZE_NUMPY).decode("utf-8")
+    outfile.write_text(serialized_init_data)
+
+    full_expected_list = []
+    for k in range(21):
+        temps = [init.T0 + i * k * init.T0 * T_std for i in range(init.row_num)]
+        full_expected_list.append(temps)
+
+    full_expected_list = F.unique_significant_floats(full_expected_list, rtol=1e-5, atol=1e-8)
+
+
+    if not validate_table_triplets_file(export.prepare_table_triplets_file, init, np.array(full_expected_list)):
+        full_expected_list_over_T0 = [x/init.T0 for x in full_expected_list]
+        table_triplets = prepare_table_triplets(init, full_expected_list_over_T0)
         output_table_triplets(table_triplets, export.prepare_table_triplets_file)
         table_val = table_triplets[:, 0]
         table_prob = table_triplets[:, 1]
@@ -78,19 +96,23 @@ def main(export: Export) -> None:
                                                            Vleft=Vleft,
                                                            repetition=0)
 
-    index = np.searchsorted(I_vec_avg, 2 * init.Amp, side="right")
-    if index < len(I_vec_avg):
-        V0 = Vleft[index]  # find voltage for the current at 2 amper
+    index_2, index_0 = np.searchsorted(I_vec_avg, 2 * init.Amp, side="right"), np.searchsorted(I_vec_avg, 0)
+    if index_2 < len(I_vec_avg):
+        V0 = Vleft[index_0]
+        V2 = Vleft[index_2]  # find voltage for the current at 2 amper
     else:
         raise ValueError("no I(V) larger than 2")
 
     ## run thermopower until I(V)<0
     current_at_V0 = True
     repetition = 0
+    V0_vec = [V0]
+    dT_vec = [0]
     while current_at_V0:
         repetition += 1
         if repetition > 20:
-            raise ValueError("too many runs")
+            print("too many runs")
+            current_at_V0 = False
 
         T_std = repetition * init.T0 / 20  # new temperature profile
 
@@ -117,26 +139,31 @@ def main(export: Export) -> None:
                                                                Vleft=Vleft,
                                                                repetition=repetition)
 
-        if I_vec_avg[index] < 0:
+        V0_vec += [np.searchsorted(I_vec_avg, 0)]
+        dT_vec += [T_std]
+
+        if I_vec_avg[index_2] < 0:
             current_at_V0 = False
 
     if export.plot_results:
         plt.plot(
-            Vleft / init.Volts,
-            I_vec_avg[:steps] / init.Amp,
-            label="increasing",
+            V0_vec / init.Volts,
+            dT_vec / init.T0,
+            label="Vth(dT) -- slope is S(T)",
             color="red",
-        )
-        plt.plot(
-            V_doubled[steps:] / init.Volts,
-            I_vec_avg[steps:] / init.Amp,
-            label="decreasing",
-            color="blue",
         )
         plt.xlabel("Voltage")
         plt.ylabel("Current")
         plt.show()
 
+    with open(f"book_{run_name}_TPgraph.csv", "w+") as f:
+        file = csv.writer(f)
+        for row in range(len(V0_vec)):
+            to_write = [
+                float(V0_vec[row] / init.Volts),
+                float(dT_vec[row] / init.T0),
+            ]
+            file.writerow(to_write)
 
 if __name__ == "__main__":
     main(

mp_compute/preparation.py
@@ -228,9 +228,7 @@ def prepare_table_triplets(init_state: ExperimentInitialState, expected_list) ->
     rr = 0
     mp.dps = 30
 
-    num_of_calc = (
-                          init_state.pos_energy_bound - init_state.neg_energy_bound
-                  ) / init_state.resolution
+    num_of_calc = (init_state.pos_energy_bound - init_state.neg_energy_bound) / init_state.resolution
     vals_to_calc = np.linspace(
         init_state.pos_energy_bound, init_state.neg_energy_bound, num=round(num_of_calc)
     )
@@ -296,7 +294,7 @@ def validate_table_triplets_file(
         else:
             print("mu = " + str(mu))
             print("Ec = " + str(init_state.Ec))
-            # ValueError("mu doesn't match Ec")
+            ValueError("mu doesn't match Ec")
     else:
         print("Existing table doesn't match T list")
         print(T_in_file)