added full expected list construction: - need to fix multiproccesing on cluster - need to build table multiprocessing computation
fixes:
-changed approximate gamma to find temp_idx in T_table and not in T_gradient, need to test:
  - in main_TPmeas table_T assignment
  - in approximate gamma, parsing via T_table
  - in approximate gamma, np.where had np.flip(T_gradient)

mp_compute/gamma_functions.py
@@ -9,13 +9,11 @@ from models import ExperimentInitialState, SteadyStateResult
 
 
 def approximate_gamma_integral(dE, T_at_junction, table_val, table_prob, T_table):
-    # for flipped
-    temp_idx = np.where(np.flip(T_table, axis=0) == T_at_junction)[0][0]
+
+    temp_idx = np.where(np.array(T_table) == T_at_junction)[0][0]
 
     sorted_vals = table_val[temp_idx::len(T_table)]
     probs = table_prob[temp_idx::len(T_table)]
-    #sorted_vals = table_val
-    #probs = table_prob
 
     idx = bisect.bisect_left(sorted_vals, dE)
 

mp_compute/main.py
@@ -41,7 +41,8 @@ def main(export: Export) -> None:
     serialized_init_data = orjson.dumps(raw_fields, option=orjson.OPT_SERIALIZE_NUMPY).decode("utf-8")
     outfile.write_text(serialized_init_data)
 
-    if not validate_table_triplets_file(export.prepare_table_triplets_file, init, np.array([init.T0 + i * init.T0 * T_std for i in range(init.row_num)])):
+    if not validate_table_triplets_file(export.prepare_table_triplets_file, init,
+                                        np.array([init.T0 + i * init.T0 * T_std for i in range(init.row_num)])):
         expected_list = [1 + i * T_std for i in range(init.row_num)]
         table_triplets = prepare_table_triplets(init, expected_list)
         output_table_triplets(table_triplets, export.prepare_table_triplets_file)
@@ -71,7 +72,9 @@ def main(export: Export) -> None:
             table_val=table_val,
             table_prob=table_prob,
             # list(reversed([init.T0 + i * init.T0 * T_std for i in range(init.row_num)])) for reversed gradient
-            T=[init.T0 + i * init.T0 * T_std for i in range(init.row_num)],
+            # if reversing T, need to unreverse for table_T
+            T=(T := [init.T0 + i * init.T0 * T_std for i in range(init.row_num)]),
+            table_T=T,
             expected_error=0.01 * (init.row_num - 1)
         )
 