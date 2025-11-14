added full expected list construction: - need to fix multiproccesing on cluster - need to build table multiprocessing computation
fixes:
-changed approximate gamma to find temp_idx in T_table and not in T_gradient, need to test:
  - in main_TPmeas table_T assignment
  - in approximate gamma, parsing via T_table
  - in approximate gamma, np.where had np.flip(T_gradient)
  -changed redundant T_list_to_compute definition
  -added flipping mechanism directly to Get_Steady_State parameters.
  -plotting with title according to flip
  -added param_output.txt output works

mp_compute/curve_plotter.py
@@ -1,16 +1,16 @@
 import csv
 
 import numpy as np
-
+import time
 from models import ExperimentInitialState
 
 
 def iv_curve_computer(
-    init: ExperimentInitialState,
-    filename: str,
-    Vleft,
-    results,
-    repetition: int,
+        init: ExperimentInitialState,
+        filename: str,
+        Vleft,
+        results,
+        repetition: int,
 ):
     V_doubled = np.concatenate([Vleft, Vleft[-2::-1]])
 
@@ -38,3 +38,53 @@ def iv_curve_computer(
             file.writerow(to_write)
 
     return I_vec_avg, I_vec_std
+
+
+def report_param(init: ExperimentInitialState,
+                 filename: str,
+                 repetition: int,
+                 T: list,
+                 expected_error: float,
+                 loop_count: int,
+                 T_std: float,
+                 t0):
+    with open(f"parameters_{filename}_rep{0}.txt", "w") as f:
+        f.write(f"repetition {repetition}\n")
+        f.write("loop parameters" + "\n")
+        f.write("---------------------------------------------" + "\n")
+        f.write("row_num : " + str(init.row_num) + "\n")
+        f.write("distribute_R : " + str(init.distribute_R) + "\n")
+        if init.distribute_R:
+            f.write("stdR (exponent) : " + str(init.stdR) + "\n")
+        f.write("distribute_C : " + str(init.distribute_C) + "\n")
+        if init.distribute_C:
+            f.write("sig (normal) : " + str(init.sig) + "\n")
+        f.write("e : " + str(init.e) + "\n")
+        f.write("C : " + str(init.C_avg) + "\n")
+        f.write("Cg : " + str(np.mean(init.Cg)) + "\n")
+        f.write("R : " + str(init.R_avg) + "\n")
+        f.write("Rg : " + str(init.CondRg) + "\n")
+        f.write("default_dt : " + str(init.default_dt) + "\n")
+        f.write("time step : " + str(init.timeStep) + "\n")
+        f.write("---------------------------------------------" + "\n")
+        f.write("\n")
+        f.write("\n")
+        f.write("loop variables : " + str(loop_count) + "\n")
+        f.write("---------------------------------------------" + "\n")
+        f.write("these are the raw variances and means\n")
+        f.write("<R> : " + str(init.R_avg) + ", std(Rt_ij) : " + str(np.std(init.R_t_ij)) + "\n")
+        f.write("<Rt_i> : "+ str(np.mean(init.R_t_i)) + ", std(Rt_i) : " + str(np.std(np.array(init.R_t_i))) + "\n")
+        f.write("<C> : " + str(init.mean_allCs) + ", std(C) : " + str(init.std_allCs) + "\n")
+        f.write("<Cix> : " + str(init.mean_sideCs) + ", std(Cix) : " + str(init.std_sideCs)+ "\n")
+        f.write("\n")
+        f.write("T0 : " + str(init.T0) + "\n")
+        f.write("T_std : " + str(T_std) + "\n")
+        f.write("T : " + str(T) + "\n")
+        f.write("---------------------------------------------" + "\n")
+        f.write("steady_state_rep : " + str(init.Steady_state_rep) + "\n")
+        f.write("expected_error : " + str(expected_error) + "\n")
+        f.write("resolution : " + str(init.resolution) + "\n")
+        end_time = time.time()
+        mins = int((end_time - t0) / 60)
+        f.write("runtime : " + str(mins) + "m\n")
+    return True

mp_compute/preparation.py
@@ -70,7 +70,7 @@ def compute_distributed_C_matrices(
         array_size: int,
         near_left: list[int],
         near_right: list[int],
-) -> tuple[npt.NDArray, npt.NDArray]:
+):
     Ch = np.random.normal(0, sig, size=(row_num, row_num + 1))
     Cv = np.random.normal(0, sig, size=(row_num + 1, row_num))
 
@@ -109,7 +109,7 @@ def compute_distributed_C_matrices(
             print(c)
             raise ValueError("Negative")
 
-    return Cix, compute_C_inverse(Ch, Cv, row_num)
+    return Cix, compute_C_inverse(Ch, Cv, row_num), np.mean(all_Cs), np.mean(side_Cs), np.std(all_Cs), np.std(side_Cs)
 
 
 def compute_fixed_C_matrices(
@@ -168,13 +168,15 @@ def prepare_initial_state(loop_count: int, unitless_T0: float) -> ExperimentInit
         R_t_i, R_t_ij = compute_fixed_R_matrices(R, array_size, near_left, near_right)
 
     if distribute_C:
-        Cix, C_inverse = compute_distributed_C_matrices(
+        Cix, C_inverse, mean_allCs, mean_sideCs, std_allCs, std_sideCs = compute_distributed_C_matrices(
             C, sig, row_num, array_size, near_left, near_right
         )
     else:
         Cix, C_inverse = compute_fixed_C_matrices(
             C, row_num, array_size, near_left, near_right
         )
+        mean_allCs, mean_sideCs = C, C
+        std_allCs, std_sideCs = 0, 0
 
     Tau_inv = define_tau_matrix(C_inverse, mean_Cg, mean_Rg, array_size)
     InvTauEigenValues, InvTauEigenVectors = np.linalg.eig(Tau_inv)
@@ -221,6 +223,12 @@ def prepare_initial_state(loop_count: int, unitless_T0: float) -> ExperimentInit
         timeStep=timeStep,
         Tau=Tau,
         matrixQnPart=Tau / (mean_Cg * mean_Rg) - np.eye(Tau.shape[0]),
+        sig=sig,
+        stdR=stdR,
+        mean_allCs=mean_allCs,
+        mean_sideCs=mean_sideCs,
+        std_allCs=std_allCs,
+        std_sideCs=std_sideCs
     )
 
 