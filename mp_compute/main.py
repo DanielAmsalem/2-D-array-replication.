added full expected list construction: - need to fix multiproccesing on cluster - need to build table multiprocessing computation
fixes:
-changed approximate gamma to find temp_idx in T_table and not in T_gradient, need to test:
  - in main_TPmeas table_T assignment
  - in approximate gamma, parsing via T_table
  - in approximate gamma, np.where had np.flip(T_gradient)
  -changed redundant T_list_to_compute definition

mp_compute/main.py
@@ -41,10 +41,10 @@ def main(export: Export) -> None:
     serialized_init_data = orjson.dumps(raw_fields, option=orjson.OPT_SERIALIZE_NUMPY).decode("utf-8")
     outfile.write_text(serialized_init_data)
 
+    T_list_to_compute = [init.T0 + i * init.T0 * T_std for i in range(init.row_num)]
     if not validate_table_triplets_file(export.prepare_table_triplets_file, init,
-                                        np.array([init.T0 + i * init.T0 * T_std for i in range(init.row_num)])):
-        expected_list = [1 + i * T_std for i in range(init.row_num)]
-        table_triplets = prepare_table_triplets(init, expected_list)
+                                        np.array(T_list_to_compute)):
+        table_triplets = prepare_table_triplets(init, T_list_to_compute)
         output_table_triplets(table_triplets, export.prepare_table_triplets_file)
         table_val = table_triplets[:, 0]
         table_prob = table_triplets[:, 1]

mp_compute/preparation.py
@@ -234,7 +234,7 @@ def prepare_table_triplets(init_state: ExperimentInitialState, expected_list) ->
     )
     rows = []
 
-    T_list_to_compute = np.array(expected_list) * init_state.T0
+    T_list_to_compute = np.array(expected_list)
     print(f"computing for energies {init_state.pos_energy_bound} > dE > {init_state.neg_energy_bound}")
     print(T_list_to_compute)
     total_to_calc = len(vals_to_calc) * len(T_list_to_compute)