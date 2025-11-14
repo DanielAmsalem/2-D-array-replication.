added full expected list construction: - need to fix multiproccesing on cluster - need to build table multiprocessing computation
fixes:
-changed approximate gamma to find temp_idx in T_table and not in T_gradient, need to test:
  - in main_TPmeas table_T assignment
  - in approximate gamma, parsing via T_table

mp_compute/gamma_functions.py
@@ -8,12 +8,12 @@ import Functions as F
 from models import ExperimentInitialState, SteadyStateResult
 
 
-def approximate_gamma_integral(dE, T_at_junction, table_val, table_prob, T_gradient):
+def approximate_gamma_integral(dE, T_at_junction, table_val, table_prob, T_table):
     # for flipped
-    temp_idx = np.where(np.flip(T_gradient, axis=0) == T_at_junction)[0][0]
+    temp_idx = np.where(np.flip(T_table, axis=0) == T_at_junction)[0][0]
 
-    sorted_vals = table_val[temp_idx::len(T_gradient)]
-    probs = table_prob[temp_idx::len(T_gradient)]
+    sorted_vals = table_val[temp_idx::len(T_table)]
+    probs = table_prob[temp_idx::len(T_table)]
     #sorted_vals = table_val
     #probs = table_prob
 
@@ -47,7 +47,7 @@ def Gamma_approx(
         pos_energy_bound,
         table_val,
         table_prob,
-        T_gradient,
+        T_table,
 ):
     # low bound starts at dE=-0.09 and thence is analytically the mean of a gaussian
     # high bound starts at dE=-0.01 and thence is 0
@@ -70,7 +70,7 @@ def Gamma_approx(
 
     elif pos_energy_bound > dE > neg_energy_bound:
         approx = approximate_gamma_integral(
-            dE, T_at_junction, table_val, table_prob, T_gradient
+            dE, T_at_junction, table_val, table_prob, T_table
         )
         val = approx * e ** 2 / Rt
         return val
@@ -128,6 +128,7 @@ def Get_Gamma(
         Ec,
         table_val,
         table_prob,
+        T_table,
 ):
     # dE values for i->j transition
     dEij = np.zeros((array_size, array_size))
@@ -166,7 +167,7 @@ def Get_Gamma(
                         pos_energy_bound,
                         table_val,
                         table_prob,
-                        T_gradient,
+                        T_table,
                     )
                 ]
                 RR += Gamma_[-1]
@@ -192,7 +193,7 @@ def Get_Gamma(
                     pos_energy_bound,
                     table_val,
                     table_prob,
-                    T_gradient,
+                    T_table,
                 )
             ]
             RR += Gamma_[-1]
@@ -217,7 +218,7 @@ def Get_Gamma(
                         pos_energy_bound,
                         table_val,
                         table_prob,
-                        T_gradient,
+                        T_table,
                     )
                 ]
                 RR += Gamma_[-1]
@@ -241,7 +242,7 @@ def Get_Gamma(
                     pos_energy_bound,
                     table_val,
                     table_prob,
-                    T_gradient,
+                    T_table,
                 )
             ]
             RR += Gamma_[-1]
@@ -265,7 +266,7 @@ def Get_Gamma(
                         pos_energy_bound,
                         table_val,
                         table_prob,
-                        T_gradient,
+                        T_table,
                     )
                 ]
                 RR += Gamma_[-1]
@@ -281,6 +282,7 @@ def Get_Steady_State(
         cycles: int,
         table_val,
         table_prob,
+        table_T,
         expected_error: float,
         T: npt.NDArray
 ):
@@ -350,6 +352,7 @@ def Get_Steady_State(
                 Ec=init.Ec,
                 table_val=table_val,
                 table_prob=table_prob,
+                T_table=table_T
             )
 
             # transition occurred, limit for R is the typical ground drain current

mp_compute/main_TPmeas.py
@@ -50,17 +50,19 @@ def main(export: Export) -> None:
 
     full_expected_list = F.unique_significant_floats(full_expected_list, rtol=1e-5, atol=1e-8)
 
-
     if not validate_table_triplets_file(export.prepare_table_triplets_file, init, np.array(full_expected_list)):
         full_expected_list_over_T0 = [x/init.T0 for x in full_expected_list]
         table_triplets = prepare_table_triplets(init, full_expected_list_over_T0)
         output_table_triplets(table_triplets, export.prepare_table_triplets_file)
         table_val = table_triplets[:, 0]
         table_prob = table_triplets[:, 1]
+        table_T = table_triplets[:len(full_expected_list), 2]
     else:
         table_triplets = np.load(export.prepare_table_triplets_file.as_posix())
         table_val = table_triplets["val"]
         table_prob = table_triplets["prob"]
+        table_T = table_triplets["temp"]
+        table_T = table_T[:len(full_expected_list)] #take only
 
     V_diff = 4
     steps = 100
@@ -81,6 +83,7 @@ def main(export: Export) -> None:
             cycles=cycles,
             table_val=table_val,
             table_prob=table_prob,
+            table_T=table_T,
             T=[init.T0],
             expected_error=0.01 * (init.row_num - 1)
         )
@@ -124,6 +127,7 @@ def main(export: Export) -> None:
                 cycles=cycles,
                 table_val=table_val,
                 table_prob=table_prob,
+                table_T=table_T,
                 T=(T := np.linspace(init.T0, init.T0 + init.row_num * T_std, init.row_num)),
                 expected_error=0.01 * (init.row_num - 1) * np.sqrt(max(T) / init.T0)
             )
@@ -139,7 +143,7 @@ def main(export: Export) -> None:
                                                                Vleft=Vleft,
                                                                repetition=repetition)
 
-        V0_vec += [np.searchsorted(I_vec_avg, 0)]
+        V0_vec += [float(np.searchsorted(I_vec_avg, 0))]
         dT_vec += [T_std]
 
         if I_vec_avg[index_2] < 0:

mp_compute/preparation.py
@@ -237,6 +237,7 @@ def prepare_table_triplets(init_state: ExperimentInitialState, expected_list) ->
     T_list_to_compute = np.array(expected_list) * init_state.T0
     print(f"computing for energies {init_state.pos_energy_bound} > dE > {init_state.neg_energy_bound}")
     print(T_list_to_compute)
+    total_to_calc = len(vals_to_calc) * len(T_list_to_compute)
 
     for val in vals_to_calc:
         for temp in T_list_to_compute:
@@ -249,10 +250,7 @@ def prepare_table_triplets(init_state: ExperimentInitialState, expected_list) ->
             )
             rr += 1
 
-            total_to_calc = len(vals_to_calc) * len(T_list_to_compute)
-            print(
-                f"done {rr} out of {total_to_calc} -- {100 * rr / total_to_calc:.2f}%"
-            )
+            print(f"done {rr} out of {total_to_calc} -- {100 * rr / total_to_calc:.2f}%")
 
     mp.dps = 15
     return np.array(rows, dtype=np.float32).reshape(-1, 4)