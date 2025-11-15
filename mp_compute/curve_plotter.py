added full expected list construction: - need to fix multiproccesing on cluster - need to build table multiprocessing computation
changes:
- line 98 preparation.py, C/2->C for Cr

mp_compute/curve_plotter.py
@@ -73,7 +73,7 @@ def report_param(init: ExperimentInitialState,
         f.write("---------------------------------------------" + "\n")
         f.write("these are the raw variances and means\n")
         f.write("<R> : " + str(init.R_avg) + ", std(Rt_ij) : " + str(np.std(init.R_t_ij)) + "\n")
-        f.write("<Rt_i> : "+ str(np.mean(init.R_t_i)) + ", std(Rt_i) : " + str(np.std(np.array(init.R_t_i))) + "\n")
+        f.write("<Rt_i> : " + str(np.mean(init.R_t_i)) + ", std(Rt_i) : " + str(np.std(np.array(init.R_t_i))) + "\n")
         f.write("<C> : " + str(init.mean_allCs) + ", std(C) : " + str(init.std_allCs) + "\n")
         f.write("<Cix> : " + str(init.mean_sideCs) + ", std(Cix) : " + str(init.std_sideCs)+ "\n")
         f.write("\n")

mp_compute/gamma_functions.py
@@ -9,7 +9,6 @@ from models import ExperimentInitialState, SteadyStateResult
 
 
 def approximate_gamma_integral(dE, T_at_junction, table_val, table_prob, T_table, flip):
-
     if flip:
         temp_idx = np.where(np.flip(T_table) == T_at_junction)[0][0]
     else:
@@ -388,6 +387,11 @@ def Get_Steady_State(
                         and zero_curr_steady_state_counter > 2
                 ):
                     not_in_steady_state = False
+
+            # check if any n is negative
+            # if F.has_neg(n):
+            #     print(n, l, m, chosen_rate)
+            #     raise ValueError
 
             # solve ODE to update Qg, dQg/dt = (T^-1)(Qg-Qn)
             Qg = F.developQ(Qg, dt, n, VxCix, init)

mp_compute/preparation.py
@@ -95,7 +95,7 @@ def compute_distributed_C_matrices(
         min_val = -np.min(side_Cs) + 0.1
 
     # Cl, Cr = Cl + max(min_val, C), Cr + max(min_val, C/2)
-    Cl, Cr = Cl + min_val + C, Cr + min_val + C / 2
+    Cl, Cr = Cl + min_val + C, Cr + min_val + C
     side_Cs = np.concatenate([Cl.ravel(), Cr.ravel()])
 
     Cix = np.zeros(array_size)