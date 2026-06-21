5f44456d71307f01ea10df45cd6a6eeb88396736
amsalda <amsalda@post.bgu.ac.il>
2026-06-21 21:41:49 +0300

    MEGA UPDATE everything in the past comits works TP plot works poly fit order 3/4 settled on and works
    updated main to get params from bash file
    
    updated for midfix:
    -table compute
    -mainTP_fixmiddle
    
    --> preparation.py had dps=40-->50
        errouneous "chunksize=10" removed
    
    --> Functions.py had np instead of mp.tanh

--- mp_compute/copy generator.py
+++ mp_compute/copy generator.py
@@ -1,8 +1,11 @@
 import shutil
+from itertools import chain
 
-for n in range(2, 20):
-    original = ("table_computer_Tstd1_20.py")
-    new_file = f"table_computer_Tstd{n}_20.py"
+for n in chain(range(1, 20, 2), [20]):
+    original = ("table_computer_fixmiddle.py")
+    new_file = f"table_computer_Tmid15_4_Tstd{n}_20_ratio1_Cg2.py"
+    shutil.copy(original, new_file)
+    new_file = f"table_computer_Tmid15_4_Tstd{n}_20_ratio1_Cg10.py"
     shutil.copy(original, new_file)
 
 print("Copies created successfully!")
